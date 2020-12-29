3a3e472a429b68b1cce5a6002f122eb761671d41
kkumarasamy <kkumarasamy@edit-place.com>
2020-12-29 19:27:31 +0530

    update db.js

--- src/db.js
+++ src/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
 import options from './config'
 
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 mongoose.set('useUnifiedTopology', true)
 
 export const connect = (url = options.dbUrl, opts = {}) => {
-  return mongoose.connect(url, { ...opts, useNewUrlParser: true })
+  return mongoose.connect(url, { ...opts })
 }
